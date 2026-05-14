d40fe847600bf2823193632eb84cbb4e04bbe818
alunounicet-lab <alunounicet@gmail.com>
2026-05-14 20:45:12 -0300

    divisao1

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 def divisao(a, b):
-    # Verifica se o divisor não é zero para evitar erro
+  
     if b == 0:
         return "Erro: Divisão por zero"
     return a / b
 
-print(divisao(6, 3)) # Resultado: 2.0
+print(divisao(6, 3))
