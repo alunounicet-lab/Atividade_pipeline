a493e05db2e003426c4a086ee6caac8704aa1f59
alunounicet-lab <alunounicet@gmail.com>
2026-05-14 20:44:13 -0300

    divisao

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,7 @@
-def soma(a, b):
-    return a + b
-    print(soma(2, 3))
+def divisao(a, b):
+    # Verifica se o divisor não é zero para evitar erro
+    if b == 0:
+        return "Erro: Divisão por zero"
+    return a / b
+
+print(divisao(6, 3)) # Resultado: 2.0
