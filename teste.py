65e9d34910a14b90b1b0c8da477d603b0aee512d
alunounicet-lab <alunounicet@gmail.com>
2026-05-14 20:21:49 -0300

    commit inicial

---
+++ teste.py
@@ -0,0 +1,10 @@
+from calculadora import soma, subtracao, multiplicacao, divisao
+
+# Testes da calculadora
+
+assert soma(2, 3) == 5
+assert subtracao(10, 4) == 6
+assert multiplicacao(3, 5) == 15
+assert divisao(8, 2) == 4
+
+print("Todos os testes foram executados com sucesso!")
