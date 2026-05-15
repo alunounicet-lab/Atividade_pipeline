1675a87f0303725ce3976cf4870cbb9b4ecc59c9
alunounicet-lab <alunounicet@gmail.com>
2026-05-15 16:54:24 -0300

    divisao

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,5 @@
 def dividir(a: float, b: float) -> float:
-    """Retorna a divisão de dois números, com tratamento de erro."""
+    
     if b == 0:
         raise ValueError("Divisão por zero não é permitida.")
     return a / b
